ae61f22dc1f2f43a1444c5fd232af859b1ad1da6
hieuvvth2009018 <73455351+hieuvvth2009018@users.noreply.github.com>
2021-12-28 13:23:39 +0700

    Add project files.

---
+++ VuonghieuASM/Controllers/ExamsController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using VuonghieuASM.Data;
+using VuonghieuASM.Models;
+
+namespace VuonghieuASM.Controllers
+{
+    public class ExamsController : Controller
+    {
+        private VuonghieuASMContext db = new VuonghieuASMContext();
+
+        // GET: Exams
+        public ActionResult Index()
+        {
+            return View(db.Exams.ToList());
+        }
+
+
+        // GET: Exams/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Exams/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,ExamSubject,StartTime,ExamDate,ExamDuration,ClassRoom,Faculty,Status")] Exam exam)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Exams.Add(exam);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(exam);
+        }
+    }
+}
---
+++ VuonghieuASM/Data/VuonghieuASMContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace VuonghieuASM.Data
+{
+    public class VuonghieuASMContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public VuonghieuASMContext() : base("name=VuonghieuASMContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<VuonghieuASM.Models.Exam> Exams { get; set; }
+    }
+}
